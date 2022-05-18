52e4bef7e2a382d9e0f62d3f8bce886f196c95df
victorocna <victorocnarescu@gmail.com>
2022-05-18 18:30:59 +0300

    simplify page

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import Head from 'next/head';
-import * as context from '../site.config';
+import * as config from '../site.config';
 import '../css/index.css';
 
 const Root = (props) => {
   const { Component, pageProps } = props;
-  const { sitename, description, resources } = context;
+  const { sitename, description, resources } = config;
 
   return (
     <>
       <Head>
         <title>{sitename}</title>
         <meta name="description" content={description} />
-        {resources.length > 0 &&
-          resources.map((href) => {
-            return <link key={href} rel="stylesheet" href={href} />;
-          })}
+        {resources.map((href) => (
+          <link key={href} rel="stylesheet" href={href} />
+        ))}
       </Head>
       <Component {...pageProps} />
     </>
